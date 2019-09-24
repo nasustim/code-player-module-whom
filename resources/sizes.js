3f85275aba3e1c7994fe6db7cb3b10ec787f6e39
nasustim <hibino17@iamas.ac.jp>
2019-09-24 16:40:46 +0900

    movie=9: 複数の検索条件に対応

--- resources/sizes.js
+++ resources/sizes.js
@@ -2,10 +2,3 @@ export const codeSize = {
   width: 768,
   height: 1024,
 }
-
-export const meSize = {
-  width: 768,
-  height: 524
-}
-
-export const meRatio = 200 / 768
--- resources/videoList.js
+++ resources/videoList.js
@@ -9,14 +9,3 @@ export const codeVideoList = [
   './c_team_team.mp4',
   './c_team_watashi.mp4',
 ];
-export const meVideoList = [
-  './m_engineer.mp4',
-  './m_seitou_seitou.mp4',
-  './m_seitou_watashi.mp4',
-  './m_kokoro_kokoro.mp4',
-  './m_kokoro_watashi.mp4',
-  './m_keiei_keiei.mp4',
-  './m_keiei_watashi.mp4',
-  './m_team_team.mp4',
-  './m_team_watashi.mp4',
-];
