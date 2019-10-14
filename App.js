bbfa810af9da1ac2100b3d0cd86c208fb96d0644
nasustim <hibino17@iamas.ac.jp>
2019-10-14 16:40:59 +0900

    add movie file option, about coding-file and my-portrait-file

--- App.js
+++ App.js
@@ -24,16 +24,16 @@ import Video from 'react-native-video';
 /**
  * Production
  */
-//import {codeVideoList} from './resources/videoList';
-//import experiences from './resources/experiences';
-//import {codeSize} from './resources/sizes';
+import {codeVideoList} from './resources/videoList';
+import experiences from './resources/experiences';
+import {codeSize} from './resources/sizes';
 
 /**
  * unitTest/coding
  */
-import {codeVideoList} from './deviceTest/unit/coding/videoList';
-import experiences from './deviceTest/unit/coding/experiences';
-import {codeSize} from './deviceTest/unit/coding/sizes';
+//import {codeVideoList} from './deviceTest/unit/coding/videoList';
+//import experiences from './deviceTest/unit/coding/experiences';
+//import {codeSize} from './deviceTest/unit/coding/sizes';
 
 /**
  * unitTest/filtering
@@ -205,7 +205,7 @@ export default class App extends Component {
             }}
           />
         </View>
-        {isPaused === false ? (
+        {isPaused === false && setting.mode == 'separate' ? (
           <Wipe isPaused={isPaused} currentTime={startTime} movieId={movieId} />
         ) : null}
       </View>
--- setting/index.js
+++ setting/index.js
@@ -2,10 +2,21 @@
  * environment:
  *  'production' || 'unitTest/coding' || 'unitTest/filtering'
  */
-const env = 'unitTest/coding';
+const env = 'production';
+//const env = 'unitTest/coding';
+
+/**
+ * mode:
+ *  playing mode
+ * 
+ *  'contain' || 'separate'
+ */
+const mode = 'contain'
+//const mode = 'separate'
 
 const setting = {
   env,
+  mode
 }
 
 export default setting
