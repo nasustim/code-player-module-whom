a1bddc6c8a20be882f926a86f992fc21a0ec28ce
nasustim <hibino17@iamas.ac.jp>
2019-11-25 22:11:15 +0900

    fix 9s bug

--- App.js
+++ App.js
@@ -67,7 +67,9 @@ export default class App extends Component {
       markerTime: -1,
       currentTime: 0,
       isPaused: setting.env === 'production',
-      rule: '',
+      rule: {
+        誰が: "*"
+      },
       isSteppable: true,
       programmerVideo: '',
       codingVideo: '',
